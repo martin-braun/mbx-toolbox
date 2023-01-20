77e4c97f8cab687c3d43f0da7971a62e75b08bfd
Martin Braun <martin.braun.1991@live.com>
2023-01-20 02:15:45 +0100

    fix: init.sh

--- lib/init.sh
+++ lib/init.sh
@@ -7,9 +7,7 @@ export MBX_LIBPATH="$MBX_PATH/lib"
 export MBX_BINPATH="$MBX_PATH/bin"
 
 # Source init.d shell scripts.
-files=( $(find "$MBX_LIBPATH/init.d" -type f -name '*.sh') )
-for f in "${files[@]}"; do
-	# echo $f
-	[ -f $f ] && { . $f || echo "- Failed to source $f"; }
-done 
+for f in $(find "$MBX_LIBPATH/init.d" -type f -name '*.sh' -maxdepth 1); do
+	\. "$f" || echo "- Failed to source $f" >&2;
+done
 
