5a2deb6008c6b45e0510a2fb33df1a7e718b66b1
Martin Braun <martin.braun.1991@live.com>
2023-02-24 01:08:57 +0100

    fix(init): find has wrong argument order

--- lib/init.sh
+++ lib/init.sh
@@ -4,7 +4,7 @@ export MBX_LIBPATH="$MBX_PATH/lib"
 export MBX_BINPATH="$MBX_PATH/bin"
 
 # Source init.d shell scripts.
-for f in $(find "$MBX_LIBPATH/init.d" -type f -name '*.sh' -maxdepth 1); do
+for f in $(find "$MBX_LIBPATH/init.d" -maxdepth 1 -type f -name '*.sh'); do
 	\. "$f" || echo "- Failed to source $f" >&2;
 done
 
